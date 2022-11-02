ea3877e5040a0c921f2207ef5de1335e2b294de8
superfish058 <11206355+superfish058@user.noreply.gitee.com>
2022-11-02 19:08:11 +0800

    update

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
-		你好
+		<p v-for="i in 1" :key="i">
+			欢迎使用hz后台管理系统
+		</p>
+		
 	</div>
 </template>
 
@@ -11,5 +14,11 @@
 </script>
 
 <style scoped>
-
+	p{
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		font-size: 22px;
+		letter-spacing: 1px;
+	}
 </style>
